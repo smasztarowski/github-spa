6a55c2e27921cfc799df55768cbd6533ba9c4d34
Sebastian Masztarowski <s.masztarowski@gmail.com>
2020-11-26 19:58:48 +0100

    feat: material-ui

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,17 @@
 import { FC } from 'react';
+import Container from '@material-ui/core/Container';
 import { Router as ReactRouter } from 'react-router-dom';
 
 import { Router } from '../Router/Router';
 
 import { browserHistory } from '../navigation/utils/history';
 
-import './App.css';
-
 export const App: FC = () => {
   return (
     <ReactRouter history={browserHistory}>
-      <Router />
+      <Container>
+        <Router />
+      </Container>
     </ReactRouter>
   );
 }
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,22 +1,29 @@
-import logo from '../logo.svg';
+import Box from '@material-ui/core/Box';
+import { DataGrid, RowsProp, ColDef } from '@material-ui/data-grid';
+
+const rows: RowsProp = [
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'XGrid', col2: 'is Awesome' },
+    { id: 3, col1: 'Material-UI', col2: 'is Amazing' },
+];
+
+const columns: ColDef[] = [
+    { field: 'col1', headerName: 'Column 1', width: 150 },
+    { field: 'col2', headerName: 'Column 2', width: 150 },
+];
 
 function Home() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+        <div>
+            <header>
+                <Box fontSize="h2.fontSize">
+                    <h2>Github Users</h2>
+                </Box>
             </header>
+
+            <div style={{ minHeight: 300, height: '100%', width: '100%' }}>
+                <DataGrid className="TEST" rows={rows} columns={columns} />
+            </div>
         </div>
     );
 };

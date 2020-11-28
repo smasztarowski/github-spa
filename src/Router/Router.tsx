6adda6e7ddd7c5e9e29f9a1626c195ed55219d02
Sebastian Masztarowski <s.masztarowski@gmail.com>
2020-11-29 00:04:56 +0100

    feat: add Back button

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -2,30 +2,16 @@ import React, { FC, Suspense } from 'react';
 import {
     Switch,
     Route as ReactRoute,
-    Link
 } from 'react-router-dom';
 
 import { Error404 } from '../Error404/Error404';
 import { Route } from './Route/Route';
 
 import { routes } from './router.routes';
-import { getRouteUrl } from './utils/routeData';
-
-import { View } from '../enums/view';
 
 export const Router: FC = () => {
     return (
         <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={getRouteUrl(View.Home)}>Home</Link>
-                    </li>
-                    <li>
-                        <Link to={getRouteUrl(View.Dashboard)}>dashboard</Link>
-                    </li>
-                </ul>
-            </nav>
             <Suspense fallback={<div>...loading</div>}>
                 <Switch>
                     {Object.entries(routes).map(([key, route]) => {

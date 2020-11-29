1642bc9ab5520c15e2cf58ccc7884f63d763dee6
Sebastian Masztarowski <s.masztarowski@gmail.com>
2020-11-29 14:05:43 +0100

    fix: improve design

--- src/GithubUserProfile/GithubUserProfile.tsx
+++ src/GithubUserProfile/GithubUserProfile.tsx
@@ -8,6 +8,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 import { useAppDispatch } from '../hooks/useAppDispatch';
@@ -26,9 +27,13 @@ const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             flexGrow: 1,
+            overflow: 'hidden',
+            padding: theme.spacing(0, 3),
         },
         paper: {
             padding: theme.spacing(2),
+            maxWidth: 400,
+            margin: `${theme.spacing(1)}px auto`,
             textAlign: 'center',
             color: theme.palette.text.secondary,
         },
@@ -91,50 +96,62 @@ export const GithubUserProfile: FC<GithubUserProfileProps> = (props) => {
                 <ArrowBack /> Users List
             </Button>
             <div className={classes.root}>
-                <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>
-                            <img style={{ width: '100%' }} src={avatar_url} alt={`${login} avatar`} />
-                        </Paper>
+                <Grid container spacing={1}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <Paper className={classes.paper}>
+                                <img style={{ width: '100%' }} src={avatar_url} alt={`${login} avatar`} />
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Paper className={classes.paper}>
+                                <Box>Bio:</Box>
+                                <Typography>{bio}</Typography>
+                            </Paper>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>
-                            <Box>Bio:</Box>
-                            {bio}
-                        </Paper>
+
+                    <Grid container spacing={3}>
+                        {email ? (
+                            <Grid item xs={3}>
+                                <Paper className={classes.paper}>
+                                    <Box>E-mail:</Box>
+                                    <Link href={email}>
+                                        <Typography noWrap>{email}</Typography>
+                                    </Link>
+                                </Paper>
+                            </Grid>
+                        ) : null}
+
+                        {html_url ? (
+                            <Grid item xs={3}>
+                                <Paper className={classes.paper}>
+                                    <Box>Github Profile:</Box>
+                                    <Link href={html_url}>
+                                        <Typography noWrap>{html_url}</Typography>
+                                    </Link>
+
+                                </Paper>
+                            </Grid>
+                        ) : null}
+
+                        {location ? (
+                            <Grid item xs={3}>
+                                <Paper className={classes.paper}>
+                                    <Box>Location:</Box>
+                                    <Box><Typography noWrap>{location}</Typography></Box>
+                                </Paper>
+                            </Grid>
+                        ) : null}
+
+                        <Grid item xs={3}>
+                            <Paper className={classes.paper}>
+                                <Box>Created at:</Box>
+                                <Box><Typography>{new Date(created_at).toDateString()}</Typography></Box>
+                            </Paper>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <Box>E-mail:</Box>
-                            {email ? (
-                                <Link href={email}>
-                                    {email}
-                                </Link>
-                            ) : null}
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <Box>Github Profile:</Box>
-                            {html_url ? (
-                                <Link href={html_url}>
-                                    {html_url}
-                                </Link>
-                            ) : null}
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <Box>Location:</Box>
-                            <Box>{location}</Box>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <Box>Created at:</Box>
-                            <Box>{new Date(created_at).toDateString()}</Box>
-                        </Paper>
-                    </Grid>
+
                 </Grid>
             </div>
         </div>

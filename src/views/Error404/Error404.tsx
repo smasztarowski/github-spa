2160d7da2caa973851909b914b6c905fa7694f95
Sebastian Masztarowski <s.masztarowski@gmail.com>
2020-11-29 14:11:19 +0100

    feat: improve 404 Page

--- src/views/Error404/Error404.tsx
+++ src/views/Error404/Error404.tsx
@@ -1,5 +1,28 @@
-import {FC} from 'react';
+import { FC, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import Button from '@material-ui/core/Button';
 
-export const Error404: FC = () => (<div>404</div>);
+import { getRouteUrl } from '../../Router/utils/routeData';
+import { View } from '../../enums/view';
+
+export const Error404: FC = () => {
+    const history = useHistory();
+
+    const handleOnClick = useCallback(() => {
+        history.push(getRouteUrl(View.Home));
+    }, [history]);
+    return (
+        <header>
+            <Button onClick={handleOnClick}>
+                <ArrowBack /> Go to Home Page
+            </Button>
+            <Box fontSize="h2.fontSize">
+                <h2>404</h2>
+            </Box>
+        </header>
+    );
+};
 
 Error404.displayName = Error404.name;
